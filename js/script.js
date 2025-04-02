305a61259e442372ec4d54a15762a2d85371d6fc
sabrina0809 <souzalaurindo49@gmail.com>
2025-04-01 22:30:38 -0300

    tops calculadoras de novo

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function expressao(parametro) {
+    console.log(parametro);
+    document.getElementById('tela').value += parametro;
+}
+
+function resultado() {
+    
+    try{
+        let expressao = document.getElementById('tela');value;
+        let resultado = eval(expressao);
+        document.getElementById('tela').value = resultado;
+    } catch (error) {
+        document.getElementById('tela').value = "erro";
+        
+
+    }
+}
